1e5ab93ee92ad33ed5cfd91b6de0b0462c73724f
Tom Childress <childressiv.dev@gmail.com>
2023-09-25 18:43:55 -0700

    Refactor: Rename and add link

--- app/src/pages/feed/[feedID].tsx
+++ app/src/pages/feed/[feedID].tsx
@@ -8,12 +8,13 @@ export default function FeedID() {
   const [data, setData] = useState<EpisodeResults | null>(null);
   const [isLoading, setLoading] = useState(true)
   const router = useRouter();
+  const feedID = router.query.feedID;
 
   useEffect(() => {
     async function fetchData() {
       const api = new APIWrapper('XRQBQ9FUEPQN2E7DQ6N5', 'qfYMb2hsc3e6ekrJe7UPbGJyWCpkyTwBJ$YDxTsV')
-      if (typeof router.query.feedID === 'string') {
-        const episodes = await api.episodesByFeedID(router.query.feedID);
+      if (typeof feedID === 'string') {
+        const episodes = await api.episodesByFeedID(feedID);
         
         setData(episodes);
         setLoading(false);
@@ -21,17 +22,17 @@ export default function FeedID() {
     }
     
     fetchData();
-  }, [router.query.feedID]);
+  }, [feedID]);
   
   if (isLoading) return <p>loading...</p>
   if (!data) return <p>No profile data</p>
 
-  if (router.query.feedID) {
+  if (feedID) {
     return (
       <>
         {data.items.map((item, index) => (
           <p>
-            <Link href={``} key={index}>
+            <Link href={`/feed/${feedID}/episode/${episodeID}`} key={index}>
               {item.title}
             </Link>
           </p>
