a046a15fc348b82377142316608a315189b7942e
Tom Childress <childressiv.dev@gmail.com>
2023-09-26 19:16:11 -0700

    Add Redux actions and reducers to [feedID].tsx

--- app/src/pages/feed/[feedID].tsx
+++ app/src/pages/feed/[feedID].tsx
@@ -1,12 +1,15 @@
-import Link from 'next/link';
-import { EpisodeResults, APIWrapper } from '../episode'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setData, setLoading } from '../../store/feedSlice';
+import { RootState } from '../../store/store';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { APIWrapper } from '../episode';
 
 export default function FeedID() {
-  const [data, setData] = useState<EpisodeResults | null>(null);
-  const [isLoading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+  const data = useSelector((state: RootState) => state.feed.data);
+  const isLoading = useSelector((state: RootState) => state.feed.isLoading);
   const router = useRouter();
   const feedID = router.query.feedID;
 
@@ -15,14 +18,14 @@ export default function FeedID() {
       const api = new APIWrapper('XRQBQ9FUEPQN2E7DQ6N5', 'qfYMb2hsc3e6ekrJe7UPbGJyWCpkyTwBJ$YDxTsV')
       if (typeof feedID === 'string') {
         const episodes = await api.episodesByFeedID(feedID);
-        
-        setData(episodes);
-        setLoading(false);
+        dispatch(setData(episodes));
+        dispatch(setLoading(false));
+        console.log(episodes.items[0].id);
       }
     }
     
     fetchData();
-  }, [feedID]);
+  }, [feedID, dispatch]);
   
   if (isLoading) return <p>loading...</p>
   if (!data) return <p>No profile data</p>
@@ -31,10 +34,9 @@ export default function FeedID() {
     return (
       <div>
         {data.items.map((item, index) => {
-        const episodeID = item.id;
         return (
           <p key={index}>
-            <Link href={`/feed/${feedID}/episode/${episodeID}`}>
+            <Link href={`/feed/${feedID}/episode/${item.id}`}>
               {item.title}
             </Link>
           </p>
