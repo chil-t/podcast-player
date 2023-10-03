661c1c2cafd820bcf680c1f98b69727ba362f320
Tom Childress <childressiv.dev@gmail.com>
2023-10-03 16:04:24 -0700

    feat: Add 'About the Project' section to About page

---
+++ app/src/pages/about.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+  <>
+    <h1>About the Project</h1>  
+    <p>
+      This project serves not only as a testament to my skills and capabilities in web development but also as a bridge to my next professional journey. Designed and built from scratch, it showcases my proficiency in handling modern web technologies and my readiness to take on more challenging roles in the industry.
+    </p>
+  </>
+  )
+}
