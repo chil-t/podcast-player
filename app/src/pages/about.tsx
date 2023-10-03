bfa1949588a8a2fc05b50ceb3e88458079378ab8
Tom Childress <childressiv.dev@gmail.com>
2023-10-03 16:21:58 -0700

    feat: Add 'Technologies used' section to About page

--- app/src/pages/about.tsx
+++ app/src/pages/about.tsx
@@ -5,6 +5,13 @@ export default function About() {
     <p>
       This project serves not only as a testament to my skills and capabilities in web development but also as a bridge to my next professional journey. Designed and built from scratch, it showcases my proficiency in handling modern web technologies and my readiness to take on more challenging roles in the industry.
     </p>
+
+    <h2>Technologies used:</h2>
+    <p>TypeScript: Provided type-safe coding, ensuring robustness and improved developer experience.</p>
+    <p>React: Enabled component-based architecture, streamlining UI development.</p>
+    <p>React-Redux: Allowed seamless state management within React components, utilizing hooks, and the Redux store for consistent state handling.</p>
+    <p>NextJS: Facilitated server-side rendering, boosting site performance and SEO.</p>
+    <p>Axios: Enhanced API calls with ease, optimizing data-fetching processes.</p>
   </>
   )
 }
